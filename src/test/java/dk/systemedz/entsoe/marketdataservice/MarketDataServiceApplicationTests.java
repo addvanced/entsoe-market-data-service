cdcae9028a2464888ef4014b6f4a0aec52bc70de
Kenneth Thomsen <contact@systemedz.com>
2022-10-16 19:27:51 +0200

    Initial Commit
    Initial Commit

---
+++ src/test/java/dk/systemedz/entsoe/marketdataservice/MarketDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.systemedz.entsoe.marketdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
