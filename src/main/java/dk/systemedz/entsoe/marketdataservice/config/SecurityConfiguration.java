b1052b7a97cd9f8c5a2f7c169ab07760724425ee
Kenneth Thomsen <contact@systemedz.com>
2022-10-22 13:23:12 +0200

    Changes security

--- src/main/java/dk/systemedz/entsoe/marketdataservice/config/SecurityConfiguration.java
+++ src/main/java/dk/systemedz/entsoe/marketdataservice/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package dk.systemedz.entsoe.marketdataservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -9,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
+    @Profile("!productionHttp")
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .requiresChannel(channel ->
