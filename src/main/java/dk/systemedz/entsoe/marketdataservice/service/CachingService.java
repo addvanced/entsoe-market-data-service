142a36780721575a7a0a1c8083fc5ace04560121
Kenneth Thomsen <contact@systemedz.com>
2022-10-24 20:14:58 +0200

    Evicting Data added

--- src/main/java/dk/systemedz/entsoe/marketdataservice/service/CachingService.java
+++ src/main/java/dk/systemedz/entsoe/marketdataservice/service/CachingService.java
@@ -2,35 +2,14 @@ package dk.systemedz.entsoe.marketdataservice.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.scheduling.annotation.Scheduled;
-
-import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
 public class CachingService {
 
-    private CacheManager cacheManager;
-
-    public void evictAllValuesFromCache(String cacheName) {
-        Objects.requireNonNull(cacheManager.getCache(cacheName.trim())).clear();
-    }
-
-    public void evictAllCaches() {
-        cacheManager.getCacheNames()
-                .parallelStream()
-                .forEach(cache -> Objects.requireNonNull(cacheManager.getCache(cache)).clear());
-    }
-
-    @Scheduled(cron = "0 23 * * * ?", zone = "Europe/Copenhagen")
-    private void evictPastYear() {
-        evictAllValuesFromCache("pastyear");
-    }
-
-    @Scheduled(cron = "0 13 5 * * ?", zone = "Europe/Copenhagen")
-    private void evictDayAhead() {
-        evictAllValuesFromCache("dayahead");
-    }
-
+    @Scheduled(cron = "10 13,23 * * * ?", zone = "Europe/Copenhagen")
+    @CacheEvict(value="pricedata", allEntries=true)
+    public void evictDayAhead() {}
 }
